de6ab44ea88578c509319fc997bef23e96df94bb
qorhod <qorhod@gmail.com>
2025-04-24 10:57:13 +0300

    Update: latest changes for deployment

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://172.20.10.13:3000/api', // استخدام Proxy بدلاً من localhost:3000
+  // baseURL: 'http://172.20.10.13:3000/api', // استخدام Proxy بدلاً من localhost:3000
+
+  baseURL: 'https://api.blokah.com', // استخدام Proxy بدلاً من localhost:3000
+
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
