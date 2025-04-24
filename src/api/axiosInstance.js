13b8dfaf9b1ec02eeddc8e1120978020d7e4c5dc
qorhod <qorhod@gmail.com>
2025-04-24 16:44:34 +0300

    add env

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://172.20.10.13:3000/api', // استخدام Proxy بدلاً من localhost:3000
+   baseURL : import.meta.env.VITE_API_BASE_URL || "http://172.20.10.13:3000/api",
 
-  baseURL: 'https://api.blokah.com', // استخدام Proxy بدلاً من localhost:3000
+  // baseURL: 'https://api.blokah.com/api', // استخدام Proxy بدلاً من localhost:3000
 
   timeout: 10000,
   headers: {
